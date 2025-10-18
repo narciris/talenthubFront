12d76ec2a7f07e4f65b0074a3c355af63456683e
narciris <narciris.mena@udea.edu.co>
2025-10-18 08:17:45 -0500

    add changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
 import { ForumListComponent } from './features/forum/components/forum-list/forum-list.component';
+import { LoginComponent } from './features/auth/pages/login/login.component';
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'forum',
+        redirectTo: 'login',
         pathMatch: 'full'
     },
        {
         // Ruta 2: Define qué componente cargar cuando la URL es /forum
-        path: 'forum',
-        component: ForumListComponent
+        path: 'login',
+        component: LoginComponent
     },
     
 ];
